3fe714d42238287eeaf77af383a85d8215985865
Hans Cappelle <hans@roov.app>
2025-09-13 11:16:44 +0200

    add http logging interceptor

--- app/src/main/java/be/hcpl/android/speedrecords/api/RetrofitInstance.kt
+++ app/src/main/java/be/hcpl/android/speedrecords/api/RetrofitInstance.kt
@@ -1,5 +1,7 @@
 package be.hcpl.android.speedrecords.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,8 +10,17 @@ object RetrofitInstance {
     private const val BASE_URL = "https://api.open-meteo.com/"
 
     fun newInstance(): Retrofit {
+
+        val intercepter = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(intercepter)
+        }.build()
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
